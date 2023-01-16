58e49f37ed98037a58b6fe67594ea85a978531c1
Ananda Krishina <anandakrishinar@gmail.com>
2023-01-16 17:39:27 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -30,6 +28,50 @@ public class Main {
         System.out.println(curso2);
         System.out.println(mentoria1);
         System.out.println(mentoria2);
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria1);
+
+        Bootcamp bootcamp2 = new Bootcamp();
+        bootcamp2.setNome("Bootcamp JS Developer");
+        bootcamp2.setDescricao("Descrição do Bootcamp JS Developer");
+        bootcamp2.getConteudos().add(curso2);
+        bootcamp2.getConteudos().add(curso1);
+        bootcamp2.getConteudos().add(mentoria2);
+
+        Dev devAnanda = new Dev();
+        devAnanda.setNome("Ananda");
+        devAnanda.inscreverBootcamp(bootcamp1);
+        System.out.println(devAnanda.getNome()+" - Conteudos inscritos: "+devAnanda.getConteudosInscritos());
+        devAnanda.progredir();
+        System.out.println(devAnanda.getNome()+" - Conteudos concluidos: "+devAnanda.getConteudosConcluidos());
+        System.out.println(devAnanda.getNome()+" - Conteudos inscritos: "+devAnanda.getConteudosInscritos());
+
+        System.out.println("__________________________________");
+        Dev devKrishina = new Dev();
+        devKrishina.setNome("Krishina");
+        devKrishina.inscreverBootcamp(bootcamp2);
+        System.out.println(devKrishina.getNome()+" - Conteudos inscritos: "+devKrishina.getConteudosInscritos());
+        devKrishina.progredir();
+        System.out.println(devKrishina.getNome()+" - Conteudos concluidos: "+devKrishina.getConteudosConcluidos());
+        System.out.println(devKrishina.getNome()+" - Conteudos inscritos: "+devKrishina.getConteudosInscritos());
+        System.out.println();
+        devKrishina.progredir();
+        System.out.println(devKrishina.getNome()+" - Conteudos concluidos: "+devKrishina.getConteudosConcluidos());
+        System.out.println(devKrishina.getNome()+" - Conteudos inscritos: "+devKrishina.getConteudosInscritos());
+        System.out.println();
+        devKrishina.progredir();
+        System.out.println(devKrishina.getNome()+" - Conteudos concluidos: "+devKrishina.getConteudosConcluidos());
+        System.out.println(devKrishina.getNome()+" - Conteudos inscritos: "+devKrishina.getConteudosInscritos());
+        System.out.println();
+        devKrishina.progredir();
+        System.out.println(devKrishina.getNome()+" - Conteudos concluidos: "+devKrishina.getConteudosConcluidos());
+        System.out.println(devKrishina.getNome()+" - Conteudos inscritos: "+devKrishina.getConteudosInscritos());
+
 
     }
 }
